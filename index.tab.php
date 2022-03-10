df79bec9facfc4ca55afe2c5e1c841fdecb9ad59
KB731 <kevinbette.formapro@gmail.com>
2022-03-10 09:27:54 +0100

    V1

---
+++ index.tab.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Les différentes manieres de declarer un tableau
+
+    // $tableau = array('valeur1', 'valeur2', 'valeur3', 'valeur4');
+
+    // $tableau[0] = 'valeur1';
+    // $tableau[1] = 'valeur2';
+    // $tableau[2] = 'valeur3';
+    // $tableau[3] = 'valeur4';
+
+    // Tableau associatif
+
+    // $tableau = array(
+    //     'prenom' => 'Kevin',
+    //     'nom' => 'Bette',
+    //     'ville' => 'Chambery',
+    //     'pays' => 'France'
+    // );
+
+    // Parcourir un tableau
+
+    // for($i = 0; $i < 3; $i++)
+    // {
+    //     echo $tableau[$i]. "<br>";   
+    // }
+
+    // foreach($tableau as $i)
+    // {
+    //         echo $i. "<br>";
+    // }
+
+    // foreach($tableau as $clef => $i)
+    // {
+    //     echo [".$clef."] = ".$i."<br>";
+    // }
+
+    // Voir dans le tableau si une clé existe
+
+    // if(array_key_exists('ville', $tableau))
+    // {
+    //     echo "OK";
+    // }else {
+    //     echo "Niet";
+    // }
+
+    // Parcourir un tableau
+
+    // $bouffe = array('burger', 'kebab', 'tacos', 'pizza');
+
+    //     if(in_array('kebab', $bouffe))
+    //     {
+    //         echo "Ok";
+    //     }else {
+    //         echo "Niet";
+    //     }
+
+    // Voir la position dans un tableau
+
+    // $bouffe = array('burger', 'kebab', 'tacos', 'pizza');
+
+    // $position = array_search('pizza', $bouffe);
+    // echo $position;
+
+    ?>
+</body>
+</html>
